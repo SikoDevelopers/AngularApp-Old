1cf59965c47af99522b24fd1ee098df95eb0c969
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-05 01:13:52 -0700

    Revert "Componente Descricao adicionado"
    This reverts commit a133686d29f624facfbb576d5ada6bab04aef2ca.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,19 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BarraDoTopoComponent } from './barra-do-topo/barra-do-topo.component';
 import {AppRoutingModule} from './app-routing.module';
 import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
-import { DescricaoComponent } from './descricao/descricao.component';
-import { ConteudoDescricaoComponent } from './descricao/conteudo-descricao/conteudo-descricao.component';
-import {ConteudoDescricaoService} from './descricao/conteudo-descricao/conteudo-descricao.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarraDoTopoComponent,
-    PaginaInicialComponent,
-    DescricaoComponent,
-    ConteudoDescricaoComponent
+    PaginaInicialComponent
   ],
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
     AppRoutingModule
   ],
-  providers: [ConteudoDescricaoService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
